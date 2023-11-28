026d8ec4101ea1e10f223577e15f99223f118834
Andrey Pavlov <buskizaru@gmail.com>
2023-11-28 19:09:27 +0300

    addBMRandBMI

--- ProjectMarathon/ProjectMarathon/BMIPage.xaml.cs
+++ ProjectMarathon/ProjectMarathon/BMIPage.xaml.cs
@@ -24,5 +24,84 @@ namespace ProjectMarathon
         {
             InitializeComponent();
         }
+
+        private void ResultBMI(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(Rost.Text, out double a) && double.TryParse(Ves.Text, out double b))
+            {
+                double bmi = CalculateBMI(a, b);
+                DisplayBMIResult(bmi);
+                UpdateProgressBar(bmi);
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректные значения роста и веса.");
+            }
+        }
+
+        private void UpdateProgressBar(double bmi)
+        {
+            bmiProgressBar.Value = bmi;
+
+            if (bmi < 18.5)
+            {
+                // Недостаточный (Оранжевый цвет)
+                SetProgressBarColor(Colors.Orange);
+            }
+            else if (bmi < 24.9)
+            {
+                // Здоровый (зеленый)
+                SetProgressBarColor(Colors.Green);
+            }
+            else if (bmi < 29.9)
+            {
+                // Избыточный (желтый)
+                SetProgressBarColor(Colors.Yellow);
+            }
+            else
+            {
+                // Ожирение (красный)
+                SetProgressBarColor(Colors.Red);
+            }
+        }
+
+        private void SetProgressBarColor(Color color)
+        {
+            SolidColorBrush brush = new SolidColorBrush(color);
+            bmiProgressBar.Foreground = brush;
+        }
+
+        private double CalculateBMI(double a, double b)
+        {
+            return b / Math.Pow(a / 100, 2);
+        }
+
+        private void DisplayBMIResult(double bmi)
+        {
+            string gender = Male.IsChecked == true ? "мужской" : "женский";
+            string result;
+            if (bmi < 18.5)
+            {
+                result = "недостаточная масса тела";
+            }
+            else if (bmi < 24.9)
+            {
+                result = "нормальная масса тела";
+            }
+            else if (bmi < 29.9)
+            {
+                result = "избыточная масса тела";
+            }
+            else
+            {
+                result = "ожирение";
+            }
+            Result.Text = $"Ваш BMI: {bmi:F2}\nПол: {gender}\nСостояние: {result}";
+        }
+
+        private void Cancel(object sender, RoutedEventArgs e)
+        {
+            MyFrame.Navigate(new NewMainWindow());
+        }
     }
 }
