ca8c59e9b338ced4c72a5df2eee007f891543c84
Makarov_Slava <zim_zimovich00@mail.ru>
2023-11-29 10:48:30 +0300

    add Mysponsor

--- ProjectMarathon/ProjectMarathon/MySponsorPage.xaml.cs
+++ ProjectMarathon/ProjectMarathon/MySponsorPage.xaml.cs
@@ -24,5 +24,9 @@ namespace ProjectMarathon
         {
             InitializeComponent();
         }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.Navigate(new NewMainWindow());
+        }
     }
 }
